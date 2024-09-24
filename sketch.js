891b98c45c5023457dab2c2226bda05b70266ca8
wwplayrff <00001119599386SP@al.educacao.sp.gov.br>
2024-09-24 15:13:15 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,28 @@
-let cor;
-let posicaoHorizontal; // x
-let posicaoVertical; // y
+let angle = 0;
+let gridSize = 5; // Number of cubes in each row and column
+let cubeSize = 40; // Size of each cube
 
 function setup() {
-  createCanvas(400, 400);
-   background(color(100,0,0));
-  cor = color(random(0,255), random(0,255), random(0,255));
-  posicaoHorizontal = 200;
-  posicaoVertical = 200;
+  createCanvas(400, 400, WEBGL);
+  noFill();
 }
 
-
 function draw() {
+  background(220);
   
-  fill(cor);
-  circle(posicaoHorizontal,posicaoVertical,50);
- 
+  let spacing = cubeSize * 1.5;
+  translate(-spacing * (gridSize - 1) / 2, -spacing * (gridSize - 1) / 2, 0);
   
-  
-  
-  if (mouseIsPressed){
-    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  for (let x = 0; x < gridSize; x++) {
+    for (let y = 0; y < gridSize; y++) {
+      push();
+      translate(x * spacing, y * spacing, 0);
+      rotateX(angle);
+      rotateY(angle * 0.3);
+      box(cubeSize);
+      pop();
+    }
   }
-    
+  
+  angle += 0.03; // Increase to rotate faster
 }
